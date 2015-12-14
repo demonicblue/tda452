6f7499d3643455ab8153881c1ef3445e9a19b4cc
Henrik Hugo <hhugo@student.chalmers.se>
2015-12-14 16:20:23 +0100

    Add differentiate button to ui

--- Calculator.hs
+++ Calculator.hs
@@ -23,13 +23,14 @@ main = do
     fx      <- mkHTML "<i>f</i>(<i>x</i>)="  -- The text "f(x)="
     input   <- mkInput 20 "x"                -- The formula input
     draw    <- mkButton "Draw graph"         -- The draw button
+    differ  <- mkButton "Differentiate"      -- The differentiate button
       -- The markup "<i>...</i>" means that the text inside should be rendered
       -- in italics.
 
     -- Layout
     formula <- mkDiv
     row formula [fx,input]
-    column documentBody [canvas,formula,draw]
+    column documentBody [canvas,formula,draw,differ]
 
     -- Styling
     setStyle documentBody "backgroundColor" "lightblue"
